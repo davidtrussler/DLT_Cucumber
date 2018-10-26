b7d2477a368a21bea4c0d7d28c30a3759353df33
David Trussler <mail@davidtrussler.net>
2018-10-26 14:29:28 +0100

    Adds second step definition to pass

--- calculator/features/step_definitions/calculator_steps.rb
+++ calculator/features/step_definitions/calculator_steps.rb
@@ -1,9 +1,10 @@
-Given("the input {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Given("the input {string}") do |input|
+  @input = input
 end
 
 When("the calculator is run") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @output = `ruby calc.rb #{@input}`
+  raise('Command failed!') unless $?.success?
 end
 
 Then("the ouptut should be {string}") do |string|
