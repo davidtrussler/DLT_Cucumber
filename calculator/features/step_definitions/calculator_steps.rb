945b89bebe5f938b682c0ed26ae6d43fa94f99de
David Trussler <mail@davidtrussler.net>
2018-10-26 14:50:31 +0100

    Adds third step definition to fail

--- calculator/features/step_definitions/calculator_steps.rb
+++ calculator/features/step_definitions/calculator_steps.rb
@@ -7,6 +7,6 @@ When("the calculator is run") do
   raise('Command failed!') unless $?.success?
 end
 
-Then("the ouptut should be {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("the ouptut should be {string}") do |expected_output|
+  @output.should == expected_output
 end
