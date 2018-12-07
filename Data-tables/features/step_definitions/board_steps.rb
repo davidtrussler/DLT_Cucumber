7df971a9c9a1e8e119eb85f82ad35a8570210b7a
David Trussler <mail@davidtrussler.net>
2018-12-07 16:44:10 +0000

    Create steps

---
+++ Data-tables/features/step_definitions/board_steps.rb
@@ -0,0 +1,13 @@
+Given /^a board like this:$/ do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When /^player x plays in row (\d+), column (\d+)$/ do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then /^the board should look like this:$/ do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
