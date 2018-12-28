c2151152aa7bc5ef88a8d5647b601914325daf16
David Trussler <mail@davidtrussler.net>
2018-12-28 12:26:29 +0000

    Defines class Account and adds first step

--- cash_withdrawal/features/step_definitions/steps.rb
+++ cash_withdrawal/features/step_definitions/steps.rb
@@ -1,5 +1,18 @@
+class Account
+  def deposit(amount)
+    @balance = amount
+  end
+
+  def balance
+    @balance
+  end
+end
+
 Given /^I have deposited \£(\d+) in my account$/ do |amount|
-  pending ('Need to design the  Account interface')
+  my_account = Account.new
+  my_account.deposit(amount)
+  my_account.balance.should eq(amount),
+    "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
 When /^I request \£(\d+)$/ do |amount|
