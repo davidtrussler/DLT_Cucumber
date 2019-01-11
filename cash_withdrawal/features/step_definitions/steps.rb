968fecaf7b6e477e593d60271e6c9b96ea9acf58
David Trussler <mail@davidtrussler.net>
2019-01-11 16:29:11 +0000

    Adds world extensions file

--- cash_withdrawal/features/step_definitions/steps.rb
+++ cash_withdrawal/features/step_definitions/steps.rb
@@ -1,19 +1,3 @@
-module KnowsTheDomain
-  def my_account
-    @my_account ||= Account.new
-  end
-
-  def cash_slot
-    @cash_slot ||= CashSlot.new
-  end
-
-  def teller
-    @teller ||= Teller.new(cash_slot)
-  end
-end
-
-World(KnowsTheDomain)
-
 Given /^I have deposited \£(\d+) in my account$/ do |amount|
   my_account.deposit(amount)
   my_account.balance.should eq(amount),
